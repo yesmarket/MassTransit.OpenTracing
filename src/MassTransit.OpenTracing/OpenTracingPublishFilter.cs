b3b22adbecbcdcc6285751d16b3701fa0e2111e9
mrxrsd <mrxrsd@gmail.com>
2021-09-17 10:31:32 -0300

    fixes publish filter when parent context span is null

--- src/MassTransit.OpenTracing/OpenTracingPublishFilter.cs
+++ src/MassTransit.OpenTracing/OpenTracingPublishFilter.cs
@@ -5,31 +5,34 @@ using OpenTracing.Util;
 
 namespace MassTransit.OpenTracing
 {
-    public class OpenTracingPublishFilter : IFilter<PublishContext>
-    {
-        public void Probe(ProbeContext context)
-        { }
+   public class OpenTracingPublishFilter : IFilter<PublishContext>
+   {
+      public void Probe(ProbeContext context)
+      { }
 
-        public async Task Send(PublishContext context, IPipe<PublishContext> next)
-        {
-            var operationName = $"Publishing Message: {context.DestinationAddress.GetExchangeName()}";
+      public async Task Send(PublishContext context, IPipe<PublishContext> next)
+      {
+         var operationName = $"Publishing Message: {context.DestinationAddress.GetExchangeName()}";
 
-            var spanBuilder = GlobalTracer.Instance.BuildSpan(operationName)
-               .AsChildOf(GlobalTracer.Instance.ActiveSpan.Context)
-               .WithTag("destination-address", context.DestinationAddress?.ToString())
-               .WithTag("source-address", context.SourceAddress?.ToString())
-               .WithTag("initiator-id", context.InitiatorId?.ToString())
-               .WithTag("message-id", context.MessageId?.ToString());
+         var spanBuilder = GlobalTracer.Instance.BuildSpan(operationName);
+         var currentCtx = GlobalTracer.Instance.ActiveSpan?.Context;
 
-            using (var scope = spanBuilder.StartActive())
-            {
-                GlobalTracer.Instance.Inject(
-                   GlobalTracer.Instance.ActiveSpan.Context,
-                   BuiltinFormats.TextMap,
-                   new MassTransitTextMapInjectAdapter(context));
+         if (currentCtx != null)
+         {
+            spanBuilder.AsChildOf(GlobalTracer.Instance.ActiveSpan.Context);
+         }
 
-                await next.Send(context);
-            }
-        }
-    }
+
+         spanBuilder.WithTag("destination-address", context.DestinationAddress?.ToString())
+                    .WithTag("source-address", context.SourceAddress?.ToString())
+                    .WithTag("initiator-id", context.InitiatorId?.ToString())
+                    .WithTag("message-id", context.MessageId?.ToString());
+
+         using (var scope = spanBuilder.StartActive())
+         {
+            GlobalTracer.Instance.Inject(scope.Span.Context, BuiltinFormats.TextMap, new MassTransitTextMapInjectAdapter(context));
+            await next.Send(context);
+         }
+      }
+   }
 }
